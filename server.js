974ac6a2ceaf4d362766ed8ed3966370815eefc8
webcrafterscollective <webcrafterscollective@gmail.com>
2024-05-16 07:10:43 +0530

    Your commit message 4

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const serveStatic = require('serve-static');
 const path = require('path');
-
 const app = express();
 
-// Serve the Flutter web app's static files
-app.use('/', serveStatic(path.join(__dirname, 'web')));
+// Serve the Flutter web build directory
+app.use(express.static(path.join(__dirname, 'web')));
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Catch-all route to serve index.html for any request
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'web/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
